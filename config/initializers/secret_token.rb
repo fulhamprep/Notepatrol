666645421a8ca3cf77be7a47439698bcd5729c48
Piers Young <piers.young@gmail.com>
2012-12-30 23:12:10 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notepatrol::Application.config.secret_token = '1e8c2923a65de6dfd8e717c2e6ac5df1ee6f6984e803c434578c4a0c4be5888a3180063643d6bc6899e55eb27b19c99a279d0381dda387e1972aa680bab24ad0'
