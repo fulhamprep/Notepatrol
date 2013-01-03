5c4f8af813356a5127692452fd2fd05763493e64
Piers Young <pyoung@fulhamprep.co.uk>
2013-01-03 20:46:49 +0000

    Tidying up login screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,37 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
+    #to tidy up login
+    layout :layout_by_resource
+
+    protected
+
+    def layout_by_resource
+        if devise_controller?
+            "devise_layout"
+        else
+            "application"
+        end
+    end
+
+
+#added for mobile
+    before_filter :prepare_for_mobile
+
+    private
+
+    def mobile_device?
+        if session[:mobile_param]
+            session[:mobile_param] == "1"
+        else
+            request.user_agent =~ /Mobile|webOS/
+        end
+    end
+    helper_method :mobile_device?
+
+    def prepare_for_mobile
+        session[:mobile_param] = params[:mobile] if params[:mobile]
+        request.format = :mobile if mobile_device?
+    end
+
 end
